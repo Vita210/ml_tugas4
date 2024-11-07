7af5957d985d300bebd29130df178fe07435d3e7
Vita210 <220711706@students.uajy.ac.id>
2024-11-07 15:49:43 +0700

    Update Tugas_Modul4_A_VITA_11706.py

--- Tugas_Modul4_A_VITA_11706.py
+++ Tugas_Modul4_A_VITA_11706.py
@@ -1,28 +1,33 @@
 import streamlit as st
 import pandas as pd
-import pickle
+import pickle 
 import os
-import plotly.express as px
+import plotly.express as px 
 import numpy as np
-from sklearn.metrics import pairwise_distances
-import plotly.graph_objects as go
+from sklearn.metrics import pairwise_distances 
+import plotly.graph_objects as go 
 
 def scatter(model, model_name, data, new_point, features, color_scale, title):
-    clusters = model.fit_predict(data[features])
-    data[f"{model_name}_Cluster"] = clusters
-
+    clusters = model.fit_predict(data[features]) #untuk memprediksi cluster untuk setiap title pada data menggunakan model
+    data[f'{model_name}_Cluster'] = clusters
+    
+    #menentukan cluster untuk titik baru
     if model_name == "KMeans_model":
+        #pada k-means, dilakukan predict langsung
         new_cluster = model.predict(new_point[features])[0]
     else:
+        #pada agglomeratv dan dbscan dilakukan dengan cara menghitung jarak titik terdekat
         distances = pairwise_distances(new_point[features], data[features])
         nearest_index = distances.argmin()
         new_cluster = clusters[nearest_index]
-
-    fig = px.scatter_3d(data, x='Avg_Credit_Limit', y='Total_Credit_Cards', z='Total_visits_online',
-                        color=f"{model_name}_Cluster", title=title, color_continuous_scale=color_scale)
     
+    #membuat grafik 3D menggunakan Plotly Express
+    fig = px.scatter_3d(data, x='Avg_Credit_Limit', y='Total_Credit_Cards', z='Total_visits_online',
+                       color=f'{model_name}_Cluster', title=title, color_continuous_scale=color_scale)
+    
+    #menambahkan titik baru pada grafik
     fig.add_trace(
-        go.scatter3d(
+        go.Scatter3d(
             x=new_point['Avg_Credit_Limit'],
             y=new_point['Total_Credit_Cards'],
             z=new_point['Total_visits_online'],
@@ -36,7 +41,7 @@ def scatter(model, model_name, data, new_point, features, color_scale, title):
 st.set_page_config(
     page_title="11706 - Unsupervised Learning",
     page_icon="📊",
-    layout="wide",
+    layout='wide',
     initial_sidebar_state="expanded",
 )
 
@@ -44,31 +49,35 @@ uploaded_file = st.sidebar.file_uploader("Upload your input CSV file", type=["cs
 
 if uploaded_file is not None:
     input_data = pd.read_csv(uploaded_file)
-    st.markdown("<h1 style='text-align: center;'>Unsupervised Learning - VITA</h1>", unsafe_allow_html=True)
+    st.markdown("<h1 style='text-align: center;'>Unsupervised Learning - Vita</h1>", unsafe_allow_html=True)
     st.dataframe(input_data)
-
+    
     model_directory = r'D:\Kuliah\SEM 5\ml\7\Unsupervised Learning (Praktek)\Unsupervised Learning (Praktek)\Tugas4_A_11706'
     model_path = {
-        "AGG_model" : os.path.join(r'AGG_model.pkl'),
-        "KMeans_model" : os.path.join(r'KMeans_model.pkl'),
-        "DBSCAN_model" : os.path.join(r'DBSCAN_model.pkl'),
+        'AGG_model' : os.path.join(r'AGG_model.pkl'),
+        'KMeans_model' : os.path.join(r'KMeans_model.pkl'),
+        'DBSCAN_model' : os.path.join(r'DBSCAN_model.pkl')
     }
-
+    
+    #load model ketiga model ke dalam dictionary
     models = {}
-
+    
     for model_name, path in model_path.items():
         if os.path.exists(path):
             with open(path, 'rb') as f:
                 models[model_name] = pickle.load(f)
         else:
             st.write(f"Model {model_name} tidak ditemukan di path : ", path)
-
+            
+    #sidebar untuk memasukkan (menginputkan) nilai untuk titik baru yang akan diprediksi clusternya
     avg_CL = st.sidebar.number_input("Average Credit Limit", 0, 100000)
     sum_CC = st.sidebar.number_input("Total Credit Cards", 0, 10)
-    sum_VO = st.sidebar.number_input("Total Visits Online", 0, 16)
-
+    sum_VO = st.sidebar.number_input("Total Visits Online", 0, 10)
+    
     if st.sidebar.button("Prediksi !"):
+        #fitur yang digunakan untuk memprediksi
         features = ['Avg_Credit_Limit', 'Total_Credit_Cards', 'Total_visits_online']
+        #memasukkan data titik baru ke dalam DataFrame
         new_point = pd.DataFrame({
             'Avg_Credit_Limit': [avg_CL],
             'Total_Credit_Cards': [sum_CC],
@@ -86,6 +95,5 @@ if uploaded_file is not None:
         
         for i, (model_name, model, title, color_scale) in enumerate(cluster_method):
             fig, new_cluster = scatter(model, model_name, input_data, new_point, features, color_scale, title)
-            with cols[i]:
-                st.plotly_chart(fig)
-                st.markdown(f"<p style='text-align: center;'>Titik Data yang baru masuk ke dalam cluster : {new_cluster}</p>", unsafe_allow_html=True)
+            st.plotly_chart(fig)
+            st.markdown(f"<p style='text-align: center;'>Titik Data yang baru masuk ke dalam cluster : {new_cluster}</p>", unsafe_allow_html=True)
